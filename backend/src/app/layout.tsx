c8d384c52595fe4edbe063b3a05c185cb2f96e54
javicod92 <javi.javier.jc@gmail.com>
2024-11-16 14:55:20 -0300

    Adding SideBar component and Layout

--- backend/src/app/layout.tsx
+++ backend/src/app/layout.tsx
@@ -1,5 +1,8 @@
-import type { Metadata } from "next";
+import { LoginButton } from "@/components/AuthButtons";
+import NavBar from "@/components/NavBar";
 import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import { auth } from "@/auth";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,14 +12,32 @@ export const metadata: Metadata = {
   description: "e-commerce",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+  if (!session) {
+    return (
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>
+          <div className="bg-zinc-900 w-screen h-screen flex items-center justify-center">
+            <LoginButton />
+          </div>
+        </body>
+      </html>
+    );
+  }
+
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        <div className="bg-zinc-800 min-h-screen flex">
+          <NavBar />
+          <main className="bg-white flex-grow p-4">{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
--- backend/src/app/page.tsx
+++ backend/src/app/page.tsx
@@ -1,15 +1,3 @@
-import { auth } from "@/auth";
-import { LoginButton } from "@/components/AuthButtons";
-
-export default async function Home() {
-  const session = await auth();
-  if (!session) {
-    return (
-      <div className="bg-zinc-900 w-screen h-screen flex items-center justify-center">
-        <LoginButton />
-      </div>
-    );
-  }
-
-  return <div className="">Logged in</div>;
+export default function page() {
+  return <div className="">page</div>;
 }
