711796c423586ccd66ed745c5fa74a63e8b19cbf
javicod92 <javi.javier.jc@gmail.com>
2024-11-18 20:43:45 -0300

    Improving multiple files upload

--- backend/src/app/api/upload/route.ts
+++ backend/src/app/api/upload/route.ts
@@ -1,10 +1,20 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: Request) {
+// for single file
+// export async function POST(request: NextRequest) {
+//   const formData = await request.formData();
+//   const file = formData.get("file") as File;
+//   const buffer = await file.arrayBuffer();
+//   // Aquí puedes procesar el buffer o guardar el archivo
+//   return NextResponse.json("ok");
+// }
+
+// for multiple files
+export async function POST(request: NextRequest) {
   const formData = await request.formData();
-  const file = formData.get("file") as File;
-  const buffer = await file.arrayBuffer();
-  console.log(buffer);
+  const files = formData.getAll("file") as File[];
+  console.log(files);
+  const buffers = await Promise.all(files.map((file) => file.arrayBuffer()));
   // Aquí puedes procesar el buffer o guardar el archivo
-  return NextResponse.json({ message: "Archivo recibido" });
+  return NextResponse.json("ok");
 }
