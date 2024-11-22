e6e0bd44bed748652077d222ae7d2ab309d3fcde
javicod92 <javi.javier.jc@gmail.com>
2024-11-22 17:51:22 -0300

    Updating category page

--- backend/src/app/categories/page.tsx
+++ backend/src/app/categories/page.tsx
@@ -1,3 +1,32 @@
+"use client";
+
+import { useState } from "react";
+
 export default function CategoriesPage() {
-  return <div>CategoriesPage</div>;
+  const [name, setName] = useState("");
+
+  function saveCategory(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+
+  return (
+    <div>
+      <h1>Categories</h1>
+      <form onSubmit={saveCategory}>
+        <fieldset className="m-0">
+          <legend>New category name</legend>
+          <input
+            type="text"
+            placeholder="Category name"
+            aria-label="category name"
+            value={name}
+            onChange={(e) => setName(e.currentTarget.value)}
+          />
+        </fieldset>
+        <button type="submit" className="btn-primary">
+          Save
+        </button>
+      </form>
+    </div>
+  );
 }
