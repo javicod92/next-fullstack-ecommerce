9c70e20bd14713f7d10238157bd77fe0ea09e002
javicod92 <javi.javier.jc@gmail.com>
2024-11-27 02:42:57 -0300

    Improving admin login

--- backend/src/auth.ts
+++ backend/src/auth.ts
@@ -2,7 +2,6 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import client from "./lib/db";
-import { NextResponse } from "next/server";
 
 const adminEmails = process.env.ADMIN_EMAIL;
 
@@ -10,7 +9,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: MongoDBAdapter(client),
   providers: [Google],
   callbacks: {
-    async session({ session, token, user }) {
+    async signIn({ user }) {
+      if (adminEmails?.includes(user.email!)) {
+        return true;
+      }
+      return false;
+    },
+    async session({ session }) {
       // Si el usuario es un administrador, retornar la sesión completa
       if (adminEmails?.includes(session?.user?.email)) {
         return session; // Sesión válida
@@ -21,6 +26,29 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     },
   },
 });
+
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+//   adapter: MongoDBAdapter(client),
+//   providers: [Google],
+//   callbacks: {
+//     async signIn({ user }) {
+//
+//       if (adminEmails?.includes(user.email!)) {
+//         return true;
+//       }
+//       return false;
+//     },
+//     async session({ session }) {
+//
+//       if (session.user && adminEmails?.includes(session.user.email!)) {
+//         session.user.isAdmin = true;
+//       } else {
+//         session.user.isAdmin = false;
+//       }
+//       return session;
+//     },
+//   },
+// });
 
 export async function isAdmin() {
   const session = await auth();
