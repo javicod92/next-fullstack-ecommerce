2feed2c7530a28ecb870c88d772f49607b5c0586
javicod92 <javi.javier.jc@gmail.com>
2024-12-18 16:30:38 -0300

    Adding Stripe to payments

--- frontend/src/app/api/checkout/route.ts
+++ frontend/src/app/api/checkout/route.ts
@@ -2,6 +2,7 @@ import { mongooseConnect } from "@/lib/mongoose";
 import { Order } from "@/models/Order";
 import { Product } from "@/models/Product";
 import { NextRequest, NextResponse } from "next/server";
+const stripe = require("stripe")(process.env.STRIPE_SK);
 
 export function GET() {
   return NextResponse.json(
@@ -29,7 +30,7 @@ export async function POST(request: NextRequest) {
         line_items.push({
           quantity,
           price_data: {
-            currency: "USD",
+            currency: "usd",
             product_data: { name: productInfo.title },
             unit_amount: quantity * productInfo.price,
           },
@@ -46,6 +47,16 @@ export async function POST(request: NextRequest) {
       streetAdress,
       country,
       paid: false,
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items,
+      mode: "payment",
+      customer_email: email,
+      success_url: process.env.PUBLIC_URL + "/cart?success=1",
+      cancel_url: process.env.PUBLIC_URL + "/cart?canceled=1",
+      metadata: { orderId: orderDoc._id.toString() },
     });
 
     return NextResponse.json(orderDoc, { status: 200 });
