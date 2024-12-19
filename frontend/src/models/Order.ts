b6dd0dde7ecc827f357a60429fde369899e92a40
javicod92 <javi.javier.jc@gmail.com>
2024-12-19 12:07:36 -0300

    Fixing error in OrderSchema, renaming postarCode to postalCode

--- frontend/src/models/Order.ts
+++ frontend/src/models/Order.ts
@@ -1,14 +1,19 @@
 import mongoose, { model, Schema, models } from "mongoose";
 
-const OrderSchema = new Schema({
-  line_items: Object,
-  name: String,
-  email: String,
-  city: String,
-  postarCode: String,
-  streetAdress: String,
-  country: String,
-  paid: Boolean,
-});
+const OrderSchema = new Schema(
+  {
+    line_items: Object,
+    name: String,
+    email: String,
+    city: String,
+    postalCode: String,
+    streetAdress: String,
+    country: String,
+    paid: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const Order = models.Order || model("Order", OrderSchema);
