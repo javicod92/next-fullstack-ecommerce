11391973caf4a781ab579ea4ba4f46019bea6468
javicod92 <javi.javier.jc@gmail.com>
2024-12-19 18:38:15 -0300

    Improving Loader component

--- backend/src/app/loading.tsx
+++ backend/src/app/loading.tsx
@@ -1,3 +1,26 @@
 export default function Loading() {
-  return <div className="w-full h-full">loading...</div>;
+  return (
+    <div className="skeleton">
+      {/* Título */}
+      <div className="title"></div>
+
+      {/* Estructura de la tabla */}
+      <div className="table">
+        {/* Encabezados de tabla */}
+        <div className="row header">
+          <div className="cell"></div>
+          <div className="cell"></div>
+          <div className="cell"></div>
+        </div>
+        {/* Filas de la tabla */}
+        {[...Array(3)].map((_, index) => (
+          <div key={index} className="row">
+            <div className="cell"></div>
+            <div className="cell"></div>
+            <div className="cell"></div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
