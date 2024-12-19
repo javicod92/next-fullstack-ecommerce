9ba63284a44d71bf84b9e8714eecfdff9a493980
javicod92 <javi.javier.jc@gmail.com>
2024-12-19 18:41:44 -0300

    Improving styles for Loading component

--- backend/src/app/loading.tsx
+++ backend/src/app/loading.tsx
@@ -1,18 +1,15 @@
 export default function Loading() {
   return (
     <div className="skeleton">
-      {/* Título */}
       <div className="title"></div>
 
-      {/* Estructura de la tabla */}
       <div className="table">
-        {/* Encabezados de tabla */}
         <div className="row header">
           <div className="cell"></div>
           <div className="cell"></div>
           <div className="cell"></div>
         </div>
-        {/* Filas de la tabla */}
+
         {[...Array(3)].map((_, index) => (
           <div key={index} className="row">
             <div className="cell"></div>
