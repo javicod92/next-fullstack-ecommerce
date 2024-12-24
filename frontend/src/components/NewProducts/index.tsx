84229a5da8cae77f4cae10f1639016bf18cfc00c
javicod92 <javi.javier.jc@gmail.com>
2024-12-24 02:01:40 -0300

    Adding motion effect to products cards using "Motion"

--- frontend/src/components/NewProducts/index.tsx
+++ frontend/src/components/NewProducts/index.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { motion } from "framer-motion";
 import ProductBox from "../ProductBox";
 
 type ProductsType = {
@@ -16,8 +19,15 @@ export default function NewProducts({
       <div className="Center">
         <h2 className={titleStyles}>{title}</h2>
         <div className="grid grid-cols-[repeat(auto-fit,minmax(150px,1fr))] gap-5 pt-5">
-          {products?.map((product) => (
-            <ProductBox key={product._id} {...product} />
+          {products?.map((product, index) => (
+            <motion.div
+              key={product._id}
+              initial={{ y: -50, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.25, delay: index * 0.025 }}
+            >
+              <ProductBox {...product} />
+            </motion.div>
           ))}
         </div>
       </div>
