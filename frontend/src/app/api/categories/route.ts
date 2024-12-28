b9883c79f054ed45ef3cd9d6da05c15e8153d596
javicod92 <javi.javier.jc@gmail.com>
2024-12-28 11:28:24 -0300

    Improving performance in categories API

--- frontend/src/app/api/categories/route.ts
+++ frontend/src/app/api/categories/route.ts
@@ -1,36 +1,43 @@
-import { NextResponse } from "next/server";
-import { Product } from "@/models/Product";
-import { Category } from "@/models/Category";
 import { mongooseConnect } from "@/lib/mongoose";
+import { Category } from "@/models/Category";
+import { Product } from "@/models/Product";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
     await mongooseConnect();
 
-    // Obtain all categories
+    // Obtener todas las categorías con su estructura de padres
     const categories = await Category.find();
-
-    // Map each category to its parent category (or to itself if it has no parent)
     const categoryParentMap = categories.reduce((map, category) => {
-      map[category._id] = category.parent || category._id; // if the category has no parent, the parent is the category itself
+      // Buscar la raíz de la jerarquía (categoría padre más alta)
+      let currentCategory = category;
+      while (currentCategory.parent) {
+        currentCategory = categories.find(
+          (cat) => cat._id.toString() === currentCategory.parent.toString()
+        );
+      }
+      map[category._id.toString()] = currentCategory._id.toString();
       return map;
     }, {});
 
-    // Products grouped by parent category
+    // Obtener todos los productos
     const products = await Product.find();
+
+    // Agrupar productos por la categoría raíz
     const groupedProducts = products.reduce((acc, product) => {
-      const parentCategoryId = categoryParentMap[product.category]; // Search parent category
+      const parentCategoryId = categoryParentMap[product.category]; // Categoría raíz
       if (!acc[parentCategoryId]) acc[parentCategoryId] = [];
-      acc[parentCategoryId].push(product); // Add product to parent category
+      acc[parentCategoryId].push(product); // Agregar producto a la categoría raíz
       return acc;
     }, {});
 
-    // Replacing parent category IDs with their names
+    // Preparar resultado final con nombres de las categorías padres
     const result = await Promise.all(
       Object.entries(groupedProducts).map(async ([parentId, products]) => {
         const parentCategory = await Category.findById(parentId).select("name");
         return {
-          category: parentCategory?.name || "No Category Name", // Parent category name
+          category: parentCategory?.name || "No Category Name", // Nombre de la categoría raíz
           products,
         };
       })
