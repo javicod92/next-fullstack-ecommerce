10a21a64d73544369ed3e1156945f7ce9d2a8250
javicod92 <javi.javier.jc@gmail.com>
2024-12-28 02:44:17 -0300

    Finishing Categories Page

--- frontend/src/app/(pages)/categories/page.tsx
+++ frontend/src/app/(pages)/categories/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 
+import ProductBox from "@/components/ProductBox";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import style from "./Categories.module.css";
+import { motion } from "motion/react";
+
+type CategoriesProps = {
+  category: string;
+  products: Array<Record<string, string>>;
+};
 
 export default function Categories() {
-  const [categories, setCategories] = useState<Array<Record<string, string>>>(
-    []
-  );
+  const [categories, setCategories] = useState<Array<CategoriesProps>>([]);
 
-  // Obtaining all categories whith their respective products
   useEffect(() => {
     async function fetchCategories() {
       try {
@@ -22,12 +27,44 @@ export default function Categories() {
     fetchCategories();
   }, []);
 
-  console.log(categories);
-
   return (
     <div className="flex justify-center">
-      <div className="Center">
+      <div className="Center mt-5 max-w-[1200px] w-full">
         <h1 className="font-semibold">Categories</h1>
+        <div className="flex flex-col gap-5 mt-5">
+          {categories.map((category, index) => (
+            <motion.div
+              key={category.category}
+              initial={{ y: -50, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.25, delay: index * 0.025 }}
+            >
+              <div
+                className="p-5 bg-white shadow-md rounded-md"
+                key={category.category}
+              >
+                <div className="border-b pb-2">
+                  <h2 className="font-semibold text-blue-800">
+                    {category.category}
+                  </h2>
+                </div>
+                {/* Contenedor con scroll horizontal */}
+                <div
+                  className={`${style.scroll_container} flex gap-5 overflow-x-auto whitespace-nowrap p-2 mt-5`}
+                >
+                  {category.products.map((product, index) => (
+                    <div className="w-full max-w-[175px] shrink-0" key={index}>
+                      <ProductBox {...product} />
+                    </div>
+                  ))}
+                </div>
+                <div className="bg-zinc-200 p-2 rounded-md mt-5 font-semibold transition-colors hover:bg-black hover:text-white">
+                  <button className="w-full">View All</button>
+                </div>
+              </div>
+            </motion.div>
+          ))}
+        </div>
       </div>
     </div>
   );
