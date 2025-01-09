5a1da9e471ad4bafc8b6be4507de3e95543c8401
javicod92 <javi.javier.jc@gmail.com>
2025-01-09 14:45:42 -0300

    Improving cart icon

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -36,7 +36,34 @@ export default function Header() {
               Account
             </Link>
             <Link onClick={() => setIsNavOpen(false)} href={"/cart"}>
-              Cart ({cartProducts.length})
+              <div>
+                <div className="hidden relative md:block">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                    />
+                  </svg>
+                  <div
+                    className={`${
+                      cartProducts.length > 0 ? "flex" : "hidden"
+                    } absolute -top-4 -right-4 w-6 h-6 bg-[#2e70ff] rounded-full text-white text-sm items-center justify-center`}
+                  >
+                    {cartProducts.length}
+                  </div>
+                </div>
+                <div className="block md:hidden">
+                  cart ({cartProducts.length})
+                </div>
+              </div>
             </Link>
           </nav>
           <div className="md:hidden leading-[0]">
